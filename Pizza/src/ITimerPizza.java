82532e208a97fef299eb7ed6593d091fcca440e5
Hasler TEHOU <47756458+octhama@users.noreply.github.com>
2024-03-09 22:54:50 +0100

    update

--- Pizza/src/ITimerPizza.java
+++ Pizza/src/ITimerPizza.java
@@ -1,12 +1,9 @@
 public interface ITimerPizza {
-    public void start();
-    public void stop();
+    public void demarrerLaCuisson();
+    public void arreterLaCuisson();
     public void pause();
-    public void resume();
-    public void reset();
+    public void reprendreLaCuisson();
+    public void annulerLaCuisson();
     public void setTimer(int seconds);
     public int getTimer();
-    public <ITimerListener> void setTimerListener(ITimerListener listener);
-    public <ITimerListener> void removeTimerListener(ITimerListener listener);
-    public void notifyTimerListener();
 }
